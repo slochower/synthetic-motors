abbf6abe80baa3854d9af46f92b87a4ef26c5769
David Slochower <slochower@gmail.com>
2018-02-14 13:01:23 -0800

    Pulled in scan results from TSCC and updating documentation

---
+++ Notes.md
@@ -0,0 +1,3 @@
+Note that @doi:10.1021/ct200199w used complete active space SCP calculations, which Gaussian can do, referenced (here)[http://gaussian.com/cas/]. I don't understand that, yet.
+
+Excited state torsion scan, described (here)[http://gaussian.com/cis/].
---
+++ gaussian-torsion-scan-HF-631Gdp/1540441.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -q home
+#PBS -N ground-state-torsion-HF-631Gdp
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=20:00:00
+#PBS -o 1540441.out
+#PBS -e 1540441.err
+#PBS -m abe
+#PBS -M slochower@gmail.com
+#PBS -V
+
+cd $PBS_O_WORKDIR
+module load gaussian
+g09 1540441.gjf
